82bf3703d4a2ebe5f4c1a758fed486ba15b3e44d
sande <sande@LAPTOP-R2BGTDD0>
2023-09-22 19:22:56 +0530

    Added testsuitedemo2

--- Test Suites/TestSuiteDemo2.ts
+++ Test Suites/TestSuiteDemo2.ts
@@ -1,25 +1,18 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>Test SuiteDemo1</name>
+   <name>TestSuiteDemo2</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
-   <pageLoadTimeout>30</pageLoadTimeout>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>7</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>true</rerunImmediately>
-   <testSuiteGuid>4b665a33-a694-424e-8b33-3cc7b6db2c1d</testSuiteGuid>
+   <testSuiteGuid>a2c5bf9d-ec79-44fd-938d-4b6dc9359c65</testSuiteGuid>
    <testCaseLink>
-      <guid>fd24b3ba-ff4d-4e31-9c3d-f2aaf7fcf01d</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
-      <testCaseId>Test Cases/Demo/ManualModeDemo</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>be5fb4bc-5cf3-45e2-89d8-db33c034bd3d</guid>
+      <guid>f7381f75-b49c-4103-916b-25c450bfabe9</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Demo/RecordModeDemo</testCaseId>
